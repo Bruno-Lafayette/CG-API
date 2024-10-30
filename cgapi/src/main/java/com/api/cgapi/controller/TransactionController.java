41439149b267831faf04611ea0dc0531aa42c88b
Bruno Lafayette <brunolafayette@rocketmail.com>
2024-10-30 15:21:09 -0300

    update data and cors

--- cgapi/src/main/java/com/api/cgapi/controller/TransactionController.java
+++ cgapi/src/main/java/com/api/cgapi/controller/TransactionController.java
@@ -3,6 +3,7 @@ package com.api.cgapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import com.api.cgapi.repository.TransactionRepository;
 
 @RestController
 @RequestMapping("/api/transactions")
+@CrossOrigin(origins = "http://localhost:3001")
 public class TransactionController {
 
     @Autowired
